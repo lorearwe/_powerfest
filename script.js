aa147ad9f6e9885fe8fa726c7f5fb7a872081907
shittyjobs <58910829+shittyjobs@users.noreply.github.com>
2020-12-17 20:57:23 +0100

    mas
    mas

--- script.js
+++ script.js
@@ -141,3 +141,22 @@ update = function() {
   requestAnimationFrame(update);
 };
 requestAnimationFrame(update);
+
+/* resaltar idioma */
+
+function changeStyle() {
+  var x = document.getElementById('en');
+  var e = document.getElementById('es');
+  x.style.fontWeight = "bold";
+  x.style.fontStyle = "italic";
+  e.style.fontWeight="normal";
+  e.style.fontStyle="normal";
+}
+function changeS() {
+  var x = document.getElementById('en');
+  var e = document.getElementById('es');
+  e.style.fontWeight = "bold";
+  e.style.fontStyle = "italic";
+  x.style.fontWeight="normal";
+  x.style.fontStyle="normal";
+}
