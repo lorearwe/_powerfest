ca0a7b3090a2e67604b5328e97ae4bb75b4ff932
shittyjobs <58910829+shittyjobs@users.noreply.github.com>
2020-12-19 20:09:48 +0100

    animacion color logo
    svg

--- js/lottie_logo.js
+++
@@ -1,8 +0,0 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('lottie'), // Required
-  path: 'logop.json', // Required
-  renderer: 'svg/canvas/html', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-  name: "Powerfest Logo", // Name for future reference. Optional.
-})
