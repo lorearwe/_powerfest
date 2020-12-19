81a5bb91eb0a72c77f18808d15529cb168405d1c
shittyjobs <58910829+shittyjobs@users.noreply.github.com>
2020-12-19 14:20:20 +0100

    svg anim
    n

---
+++ js/lottie_logo.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('lottie'), // Required
+  path: 'logop.json', // Required
+  renderer: 'svg/canvas/html', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Powerfest Logo", // Name for future reference. Optional.
+})
